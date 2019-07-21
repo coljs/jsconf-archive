ed1dfa1000d276ae4f91a5da8695230d0edf5f78
David Avellaneda <g.avella@gmail.com>
2019-07-21 11:26:23 +0200

    Add 2017 (not working properly)

--- scraper.js
+++ scraper.js
@@ -1,10 +1,10 @@
 var scrape = require('website-scraper')
 var options = {
-  urls: ['https://jsconf.co/'],
+  urls: ['http://2017.jsconf.co/'],
   directory: './2017/',
 }
- 
-// with callback 
+
+// with callback
 scrape(options, function (error, result) {
   console.log(result)
 })
